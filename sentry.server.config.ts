693155bc3492556fbe3a3bc12cbc2352b6644d12
amirrezajm <amirreza.jolani@yahoo.com>
2026-05-12 18:13:09 +0200

    feat: implement voices feature with creation dialog, playback, and Sentry integration

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://74d9bcb64d34f8c0bd44eaa7640955e9@o4510924783157248.ingest.de.sentry.io/4511372369199184",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
